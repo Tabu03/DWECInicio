8d16e705e084c07771808a299f27b4a0c1f350a5
Tabu <tabulete@gmail.com>
2022-09-14 19:31:50 +0200

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+let pnum1 = parseFloat (prompt("Introduce el primer numero"));
+let pnum2 = parseFloat (prompt("Introduce el segundo numero"));
+
+alert(pnum1+pnum2);
+
+console.log(pnum1+pnum2);
+
+document.write(pnum1+pnum2);
