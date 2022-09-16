a20128b4108f15359eab93f83a30d89301b571e8
Tabu <tabulete@gmail.com>
2022-09-16 21:13:06 +0200

    commit 4

--- js/script.js
+++ js/script.js
@@ -98,26 +98,87 @@
 // }
 // console.log(total)
 
-let nombre = "";
-let apellido = "";
-let horas;
-let precio;
-let total;
-let hextra;
-let precioExtra;
 
-for (let i = 0; i < 5; i++) {
-    nombre = prompt("Nombre del trabajador:","");
-    apellido = prompt("Apellido del trabajador:","");
-    horas = parseInt(prompt("Introduce el numero de horas:"));
-    precio = parseInt(prompt("Introduce el precio de las horas:"));
-    if (horas <= 40) {
-        total = precio * horas;
-    }
-    else {
-        hextra = horas - 40;
-        precioExtra = hextra * (precio * 1.5)
-        total = (precio * 40) + precioExtra;
-    }
-    console.log(nombre+" "+apellido+": "+total)
-}
+
+// for (let i = 0; i < 5; i++) {
+
+//     let nombre = "";
+//     let apellido = "";
+//     let horas;
+//     let precio;
+//     let total = 0;
+//     let precioExtra;
+
+//     nombre = prompt("Nombre del trabajador:", "");
+//     apellido = prompt("Apellido del trabajador:", "");
+//     horas = parseInt(prompt("Introduce el numero de horas:"));
+//     precio = parseInt(prompt("Introduce el precio de las horas:"));
+
+//     if (horas <= 40) {
+//         total = precio * horas;
+//     }
+//     else {
+//         precioExtra = (horas - 40) * (precio * 1.5)
+//         total = precio * 40 + precioExtra;
+//     }
+//     console.log(nombre + " " + apellido + ": " + total)
+//     console.log(`${nombre} ${apellido}: ${total}`)
+// }
+
+//EJ 4
+
+// let num1 = prompt("Introduce el primer numero");
+// let num2 = prompt("Introduce el segundo numero");
+// let cont = 0;
+
+// if(num1%2!=0 && num2!=0 && isNaN(num1)==false && isNaN(num2)==false){
+//     for (let i = num1; i < num2; i++) {
+//         if(i%2!=0){
+//             cont++;
+//         }
+
+
+//     }
+//     if(num1%2!=0){
+//         cont--;
+//     }
+//     console.log(cont);
+// }
+
+//EJ 5
+
+// let base = prompt("Introduce el primer numero");
+// let exp = prompt("Introduce el segundo numero");
+// let acu = 1;
+
+// if(!isNaN(base) && !isNaN(exp) && base>0 && exp>0){
+//     for(let i = 1; i<exp;i++){
+//         acu=acu*base;
+//         acu*=base;
+//     }
+//     console.log(acu)
+
+// }
+
+// EJ 6
+
+// let num = parseInt(prompt("Introduce"))
+
+// if (!isNaN(num) && num > 0 && num < 10) {
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(num * i);
+//     }
+// }
+
+
+
+// const sumar = (num1, num2)=>{
+//     return num1+num2
+// }
+
+// console.log(sumar(5,6));
+
+const sumar = (num1, num2)=>num1+num2
+
+
+console.log(sumar(5,6));
